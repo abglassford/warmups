f05836d63822830037e1df88514f798b552be024
Alexander Glassford <a.b.glassford@gmail.com>
2016-07-11 10:55:10 -0600

    refactored function to fail faster

--- lettercount.js
+++ lettercount.js
@@ -1,15 +1,15 @@
-function letterCount(word, letter){
-  var count = 0
-  for(var i = 0; i < word.length; i++){
-    if(word[i].toLowerCase() === letter.toLowerCase()){
-      count++
+function letterCount(word, letter) {
+  if (word.includes(letter.toLowerCase())) {
+    var count = 0;
+    for (var i = 0; i < word.length; i++) {
+      if (word[i].toLowerCase() === letter.toLowerCase()) {
+        count++;
+      }
     }
-  }
-  if(count === 0){
-    console.log('Not Found')
-  }else{
     console.log(count);
+  } else {
+    console.log('Not Found');
   }
 }
 
-letterCount('Taco xxxxx bell is terrible for you.', 'X')
+letterCount('a;lsfjlasjdflkjsadlf xxxXX', 'x')
